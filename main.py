83dfc05b5a5d6ec1c1a2d68926ba02c57a7d961d
MarkKihlthau <37964646+MarkKihlthau@users.noreply.github.com>
2019-11-21 14:41:11 -0800

    Added newEmployee

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 import mysql.connector
+import sys
+
+def newEmployee():
+    print("Enter employee name: ")
+    for line in sys.stdin:
+        name = line
+        break
+    print("Enter employee position (Cashier, Bagger, Stocker, Manager): ")
+    for line in sys.stdin:
+        position = line
+        break
+    print("Enter ID of the employing store: ")
+    for line in sys.stdin:
+        works_at = line
+        break
+    print("Enter ID of manager: ")
+    for line in sys.stdin:
+        manager_id = line
+        break
+    print("Enter employee salary: ")
+    for line in sys.stdin:
+        salary = line
+        break
+    print("Enter employee phone number: ")
+    for line in sys.stdin:
+        phone = line
+        break
+    sql = "INSERT INTO employee (name, position, works_at, manager_id, salary, phone) VALUES (%s, %s, %s, %s, %s, %s)"
+    val = (name, position, works_at, manager_id, salary, phone)
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+    return;
 
 mydb = mysql.connector.connect(user='root', password='Turing!1001',
                               host='localhost',
@@ -7,10 +40,17 @@ mydb = mysql.connector.connect(user='root', password='Turing!1001',
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT * FROM supermarket")
+mycursor.execute("SELECT * FROM employee")
 
 myresult = mycursor.fetchall()
 
 for x in myresult:
   print(x)
+
+print("Enter: ")
+for line in sys.stdin:
+    print(line)
+    break
+
+newEmployee()
 
